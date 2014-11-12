50db02c37f86e4fd413f9f497704343d325b8af3
Marcus <marma148@student.liu.se>
2014-11-12 21:16:58 +0100

    tjena

--- test2/test2.java
+++ test2/test2.java
@@ -1,5 +1,18 @@
+import java.util.LinkedList;
+
 /**
  * Created by Marcus on 14-11-12.
  */
 public class test2 {
+    public static void main (String[] args){
+
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        LinkedList<Integer> linkedList1 = new LinkedList<Integer>();
+        linkedList.add(5);
+        System.out.println(linkedList.getFirst());
+
+
+        System.out.println("Tjena Världen");
+
+    }
 }
